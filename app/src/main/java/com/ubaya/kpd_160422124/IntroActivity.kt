157bbfc1293a3622a675405afda5bd5ca1167c50
nathsteve13 <nathanaelsteven06@gmail.com>
2024-10-10 11:27:42 +0700

    week7 done

--- app/src/main/java/com/ubaya/kpd_160422124/IntroActivity.kt
+++ app/src/main/java/com/ubaya/kpd_160422124/IntroActivity.kt
@@ -1,13 +1,22 @@
 package com.ubaya.kpd_160422124
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.DatePicker
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.ubaya.kpd_160422124.databinding.ActivityIntroBinding
 import com.ubaya.kpd_160422124.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
+import java.util.Calendar
+import java.util.Locale
 
 class IntroActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroBinding
@@ -15,6 +24,53 @@ class IntroActivity : AppCompatActivity() {
     companion object {
         val PLAYER_NAME = "player_name"
     }
+
+    private fun showDatePickerDialog() {
+        val calendar: Calendar = Calendar.getInstance()
+        val year: Int = calendar.get(Calendar.YEAR)
+        val month: Int = calendar.get(Calendar.MONTH)
+        val day: Int = calendar.get(Calendar.DAY_OF_MONTH)
+
+        val datePickerDialog = DatePickerDialog(
+            this,
+            { view: DatePicker?, selectedYear: Int, selectedMonth: Int, selectedDay: Int ->
+
+                val selectedDate = Calendar.getInstance()
+                selectedDate.set(selectedYear, selectedMonth, selectedDay)
+                val displayFormat = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault())
+                val displayDate = displayFormat.format(selectedDate.time)
+                binding.txtBod.setText(displayDate)
+
+                val storageFormat = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
+                val storedDate = storageFormat.format(selectedDate.time)
+                binding.txtBod.tag = storedDate
+
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun loadMovie() {
+        binding.webView.settings.javaScriptEnabled = true
+
+        binding.webView.webViewClient = object: WebViewClient() {
+            override fun onPageFinished(view: WebView?, url: String?) {
+                super.onPageFinished(view, url)
+                binding.progressWebView.visibility = View.INVISIBLE
+            }
+        }
+
+        val videoId = "es7W-hGH99M?si=1Pc16JeuBnHMkr8k"
+        val youtubeUrl = "https://www.youtube.com/embed/$videoId"
+
+        binding.webView.loadUrl(youtubeUrl)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,6 +90,9 @@ class IntroActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        binding.txtBod.setOnClickListener { showDatePickerDialog() }
+
+        loadMovie()
 //        setContentView(R.layout.activity_intro)
     }
 }
--- app/src/main/java/com/ubaya/kpd_160422124/ResultActivity.kt
+++ app/src/main/java/com/ubaya/kpd_160422124/ResultActivity.kt
@@ -1,34 +1,37 @@
 package com.ubaya.kpd_160422124
 
 import android.content.Intent
+import android.content.SharedPreferences
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.ubaya.kpd_160422124.databinding.ActivityMainBinding
 import com.ubaya.kpd_160422124.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
+    private lateinit var sharedPreferences: SharedPreferences
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val arrayListScore = arrayListOf<Int>()
-        val endScore = intent.getIntExtra("", MainActivity.curScore)
+        sharedPreferences = getSharedPreferences("game_prefs", MODE_PRIVATE)
 
-        arrayListScore.add(endScore)
-        val highScore = arrayListScore.maxOrNull()
-
+        val endScore = intent.getIntExtra("curScore", 0)
         binding.txtScore.text = endScore.toString()
-        binding.txtHighScore.text = highScore.toString()
+
+        val highScore = sharedPreferences.getInt("high_score", 0)
+
+        if (endScore > highScore) {
+            sharedPreferences.edit().putInt("high_score", endScore).apply()
+        }
+
+        val updatedHighScore = sharedPreferences.getInt("high_score", 0)
+        binding.txtHighScore.text = updatedHighScore.toString()
 
         binding.btnReplay.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
